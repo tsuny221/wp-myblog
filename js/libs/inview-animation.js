deed1650bb883e3918392919a091a15dcf5935d3
tsuny221 <kingoftsuny@gmail.com>
2020-09-25 12:56:07 +0900

    [Update] design

--- js/libs/inview-animation.js
+++ js/libs/inview-animation.js
@@ -1,3 +1,18 @@
+//フェードイン
 $(function () {
   $(".fade-in").addClass("inview");
+});
+
+//aside/headerのフェードイン
+$(window).scroll(function () {
+  // this(window要素)がどれだけスクロールしたかをscrollTop()を使って値を取る
+  $(this).scrollTop();
+  if ($(this).scrollTop() > $(".slider").height()) {
+    $("header").css({ cssText: "opacity: 1 !important;" });
+    $(".side").addClass("inview");
+  } else {
+    $("header").css({ cssText: "opacity: 0 !important;" });
+    $("body").removeClass("menu-open");
+    $(".side").removeClass("inview");
+  }
 });
--- js/libs/mobile-menu.js
+++ js/libs/mobile-menu.js
@@ -2,13 +2,3 @@
 $(".mobile-menu__btn").on("click", function () {
   $("body").toggleClass("menu-open");
 });
-
-$(window).scroll(function () {
-  // this(window要素)がどれだけスクロールしたかをscrollTop()を使って値を取る
-  $(this).scrollTop();
-  if ($(this).scrollTop() > 300) {
-    $(".mobile-menu__btn").fadeIn();
-  } else {
-    $(".mobile-menu__btn").fadeOut();
-  }
-});
